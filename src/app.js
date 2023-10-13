947dc3175a688310a224f2bc64bae6b27fbf5a92
saraiesco <saraiescobar@google.com>
2023-10-12 22:08:50 -0500

    get request

--- src/app.js
+++ src/app.js
@@ -4,7 +4,10 @@ const Restaurant = require("../models/index")
 const db = require("../db/connection");
 
 //TODO: Create your GET Request Route Below: 
-
+app.get('/restaurants',async (request, response)=>{
+    let yum = await Restaurant.findAll({});
+    response.send(yum);
+})
 
 
 
