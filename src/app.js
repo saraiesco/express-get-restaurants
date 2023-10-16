30d6bb91564f8dca2750881f5f1375a5d63af83a
saraiesco <saraiescobar@google.com>
2023-10-16 17:42:00 -0500

    completed part 3 resturants

--- src/app.js
+++ src/app.js
@@ -16,25 +16,54 @@ app.get('/restaurants/:id',async (request, response)=>{
     response.json(rest1);
 })
 
-// app.use(express.json())
-// app.use(express.urlencoded())
 
 // //part 3
-// app.post('/restaurants/:id',async (request, response)=>{
-//     // The information that you create (i.e. POST) will be contained in 
-//     // the body of the request trying to create the resource.
-// })
 
-// app.put('/restaurants/:id',async (request, response)=>{
-//     // The information that you update (i.e. PUT) will be contained 
-//     // in the body of the request trying to create the resource and be 
-//     // used on a specific restaurant through the route parameter of the 
-//     // request.
-// })
+app.use(express.json())
+app.use(express.urlencoded())
 
-// app.delete('/restaurants/:id',async (request, response)=>{
-//     // The information that you will delete will be contained in
-//     //  the route parameter of the request.
-// })
+app.post('/restaurants',async (request, response)=>{
+    // The information that you create (i.e. POST) will be contained in 
+    // the body of the request trying to create the resource.
+    try{
+        const newRes = await Restaurant.create(request.body);
+        if (!newRes) {
+            throw new Error("Didn't create restaurant")
+        }
+        response.json(newRes)
+    } catch (err){
+        next(err);
+    }
+})
+
+app.put('/restaurants/:id',async (request, response)=>{
+    // The information that you update (i.e. PUT) will be contained 
+    // in the body of the request trying to create the resource and be 
+    // used on a specific restaurant through the route parameter of the 
+    // request.
+    try{
+        const updatedRes = await Restaurant.update(request.body,{where: {id :request.params.id}});
+        if (updatedRes[0] === 0) {
+            throw new Error("Didn't update restaurant")
+        }
+        response.sendStatus(200)
+    } catch (err){
+        next(err);
+    }
+})
+
+app.delete('/restaurants/:id',async (request, response)=>{
+    // The information that you will delete will be contained in
+    //  the route parameter of the request.
+    try{
+        const deleteRes = await Restaurant.destroy({where: {id : request.params.id}});
+        if (deleteRes === 0) {
+            throw new Error("Didn't delete restaurant")
+        }
+        response.sendStatus(200)
+    } catch (err){
+        next(err);
+    }
+})
 
 module.exports = app;
