f5a3d6a3d09a847237ccb63ec7ca4f7da3661462
saraiesco <saraiescobar@google.com>
2023-10-16 17:31:25 -0500

    correct part 2

--- src/app.js
+++ src/app.js
@@ -9,6 +9,32 @@ app.get('/restaurants',async (request, response)=>{
     response.send(yum);
 })
 
+//part 2
+app.get('/restaurants/:id',async (request, response)=>{
+    let id = request.params.id;
+    let rest1 = await Restaurant.findByPk(id)
+    response.json(rest1);
+})
 
+// app.use(express.json())
+// app.use(express.urlencoded())
+
+// //part 3
+// app.post('/restaurants/:id',async (request, response)=>{
+//     // The information that you create (i.e. POST) will be contained in 
+//     // the body of the request trying to create the resource.
+// })
+
+// app.put('/restaurants/:id',async (request, response)=>{
+//     // The information that you update (i.e. PUT) will be contained 
+//     // in the body of the request trying to create the resource and be 
+//     // used on a specific restaurant through the route parameter of the 
+//     // request.
+// })
+
+// app.delete('/restaurants/:id',async (request, response)=>{
+//     // The information that you will delete will be contained in
+//     //  the route parameter of the request.
+// })
 
 module.exports = app;
